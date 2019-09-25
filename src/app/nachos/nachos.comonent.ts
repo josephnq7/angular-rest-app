1d2d94bd0444961e8935dff939fff3e3e00cece6
josephnguyen <quanghiepk7xl@gmail.com>
2019-09-25 15:35:48 -0700

    joseph-00: play around with components

---
+++ src/app/nachos/nachos.comonent.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-nachos',
+  templateUrl: './nachos.comonent.html',
+  styleUrls: ['./nachos.comonent.css']
+})
+export class NachosComonent {
+
+}
